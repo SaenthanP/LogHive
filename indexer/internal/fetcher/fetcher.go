feef9789c2f0a296eeeb22ab4e59a651b8a5f517
Saenthan <saenthan.p@gmail.com>
2025-09-21 12:17:59 -0400

    simple go worker, block fetcher, and transaction parser

--- indexer/internal/fetcher/fetcher.go
+++ indexer/internal/fetcher/fetcher.go
@@ -1,6 +1,12 @@
 package fetcher
 
 import (
+	"context"
+	"math/big"
+
+	"log/slog"
+
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -10,4 +16,47 @@ type Fetcher struct {
 
 func NewFetcher(ethClient *ethclient.Client) *Fetcher {
 	return &Fetcher{ethclient: ethClient}
+}
+
+func (fs *Fetcher) SubscribeToLatestBlock(ctx context.Context) error {
+	headers := make(chan *types.Header, 64)
+
+	sub, err := fs.ethclient.SubscribeNewHead(ctx, headers)
+	if err != nil {
+		slog.Error("failed to subscribe to current blockchain head notification", "err", err)
+		return err
+	}
+	defer sub.Unsubscribe()
+	slog.Info("Listening to new blocks")
+	jobs := make(chan *big.Int, 1000)
+	go fs.transactionWorker(ctx, 1, jobs)
+	// TODO add graceful shutdowns
+	for {
+		select {
+		case err := <-sub.Err():
+			slog.Error("subscription error", "err", err)
+		case header := <-headers:
+			slog.Info("fetched new block", "block_number", header.Number.String())
+			jobs <- header.Number
+
+		}
+	}
+	return nil
+}
+
+// TODO switch this to a custom internal library with worker pool, handling panics, retries, and exponential backoffs, spinning up variabling up and down based on load
+func (fs *Fetcher) transactionWorker(ctx context.Context, id int, jobs <-chan *big.Int) {
+	for headerNumber := range jobs {
+		// TODO handle block reorgs, roll back? Keep in memory?
+		block, err := fs.ethclient.BlockByNumber(ctx, headerNumber)
+		if err != nil {
+			slog.Error("failed to fetch block", "err", err)
+			continue
+		}
+
+		// TODO, add any filters to noisy transactions, and send as batches with only useful info to kafka topic
+		for _, tx := range block.Transactions() {
+			slog.Info("transaction", "transaction_id", tx.Hash())
+		}
+	}
 }
--- indexer/internal/pipeline/pipeline.go
+++ indexer/internal/pipeline/pipeline.go
@@ -1,16 +1,26 @@
 package pipeline
 
+import (
+	"context"
+	"log/slog"
+)
+
 type Fetcher interface {
+	SubscribeToLatestBlock(ctx context.Context) error
 }
 
 type Pipeline struct {
-	fetcher *Fetcher
+	fetcher Fetcher
 }
 
 func NewPipeline(fetcher Fetcher) *Pipeline {
-	return &Pipeline{fetcher: &fetcher}
+	return &Pipeline{fetcher: fetcher}
 }
 
-func (pipeline *Pipeline) Run() error {
+func (pipeline *Pipeline) Run(ctx context.Context) error {
+	err := pipeline.fetcher.SubscribeToLatestBlock(ctx)
+	if err != nil {
+		slog.Error("failed to subscribe to latest block", "err", err)
+	} 
 	return nil
 }
